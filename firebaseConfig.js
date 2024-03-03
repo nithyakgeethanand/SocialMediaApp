d5f47a2509d9888f3617612e88e541e19c1b1695
nithyakgeethanand <67506979+nithyakgeethanand@users.noreply.github.com>
2024-03-03 15:10:43 +0530

    Fixed issue with fetching data from firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,4 +1,7 @@
 import { initializeApp } from 'firebase/app';
+import firebase from 'firebase/compat/app';
+import  'firebase/compat/auth';
+import  'firebase/compat/firestore';
 
 // Optionally import the services that you want to use
 // import {...} from "firebase/auth";
@@ -17,7 +20,7 @@ const firebaseConfig = {
   appId: "1:719329131393:web:6a63f7049e6fc853368680"
 };
 
-export const app = initializeApp(firebaseConfig);
+export const app = firebase.initializeApp(firebaseConfig);
 
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
--- screens/Home.js
+++ screens/Home.js
@@ -1,31 +1,69 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, Image } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import Footer from '../components/Footer';
 import { Fontisto } from '@expo/vector-icons';
+import { app } from '../firebaseConfig';
+import { screenWidth } from '../constants/dimensions';
 
 const Home = ({ route }) => {
   const navigation = useNavigation();
   const { name } = route.params;
+  const [jsonData, setJsonData] = useState({});
 
-  console.log(name);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const feedsRef = app.firestore().collection('feed');
+      const querySnapshot = await feedsRef.get();
+      let newData = {};
+
+      querySnapshot.forEach((doc) => {
+        newData[doc.id] = doc.data();
+      });
+
+      setJsonData(newData);
+    } catch (error) {
+      console.error("Error getting documents: ", error);
+    }
+  };
+
+  const renderFeedItem = ({ item }) => (
+    <View style={{ padding: 20, borderBottomWidth: 1, borderBottomColor: '#ccc', }}>
+      <View style={{ backgroundColor: '#ffffff', borderRadius: 10, padding: 10 }}>
+        <Image
+          source={{ uri: item.imageURL }}
+          style={{ width: screenWidth - 60, height: 200, borderRadius: 10, alignItems: "center", justifyContent: "center", }}
+        />
+        <View style={{ flex: 1, }}>
+          <Text style={{ fontSize: 18, paddingTop: 10 }}>{item.content}</Text>
+        </View>
+      </View>
+
+
+    </View>
+  );
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={()=> navigation.navigate("NewFeed")}>
+      <TouchableOpacity onPress={() => navigation.navigate("NewFeed")}>
         <View style={styles.plusIconContainer}>
           <Fontisto name="plus-a" style={styles.plusIcon} />
         </View>
       </TouchableOpacity>
-
-      <View style={styles.content}>
-        <Text>{name}</Text>
-      </View>
+      <FlatList
+        data={Object.values(jsonData)}
+        renderItem={renderFeedItem}
+        keyExtractor={item => item.userId}
+      />
       <Footer userName={name} />
     </View>
+  );
 
-  )
-}
+};
 
 export default Home
 
